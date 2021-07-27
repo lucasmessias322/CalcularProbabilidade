d1963c3999bd3a16421212b737fc34c4a80459d2
Lucasmessias322 <lucasmessiaspereira322@gmail.com>
2021-07-27 14:29:39 -0300

    separado logica em outro arquivo

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 
+function CalcularProbabilidade() {
+    let result = document.querySelector('#result')
+    let X = document.querySelector('#X')
+    let Y = document.querySelector('#Y')
+    let percentCalc = document.querySelector('#percentCalc')
+    //Coletar valores do inputs
+    const valores = {
+        Favoravel: document.getElementById('Favoravel').value,
+        total: document.getElementById('total').value
+    }
+
+
+    //trasnformalos em numeros
+    total = parseFloat(valores.total)
+    Favoravel = parseFloat(valores.Favoravel)
+
+
+
+    let P = (Favoravel / total);
+
+    let calcPercent = (P * 100);
+
+    X.innerHTML = Favoravel
+    Y.innerHTML = total
+
+    percentCalc.innerHTML = `${P} x 100`;
+
+    result.innerHTML = (`= ${calcPercent}%`);
+}
 
